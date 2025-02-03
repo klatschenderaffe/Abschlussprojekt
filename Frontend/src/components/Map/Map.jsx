591a1625cda4c164af2d79147dc2da3766154cea
Pia <piaheiss@gmx.de>
2025-02-03 20:32:10 +0100

    refactor(Map.jsx): Kommentar hinzugefügt. Das Pipeline ausgelöst wird

--- Frontend/src/components/Map/Map.jsx
+++ Frontend/src/components/Map/Map.jsx
@@ -3,6 +3,7 @@ import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './Map.css';
 import icon from '../../assets/markericon.png';
+// Sollen Später in MongoDB
 import locations from '../../../../Backend/sleepspots.json';
 
 const Map = () => {
