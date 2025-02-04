ee9a54e7e9d424e9a68fc3cbb1bbe3c3d767bc19
Pia <piaheiss@gmx.de>
2025-02-04 10:21:08 +0100

    fix(output.tf): * in den Values ergänzt -> in der ini File kommt die IP-Adresse noch nicht an.

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,5 +1,5 @@
 # Ausgabe der öffentlichen IPs der Instanzen
 output "public_ips" {
-  value       = data.aws_instances.frontend_instances.public_ips
+  value       = data.aws_instances.frontend_instances.*.public_ips
   description = "Öffentliche IP-Adressen der EC2-Instanzen im Frontend"
 }
