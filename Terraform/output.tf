be2616b4eef554debb80b7a95314f0e528d756b1
Pia <piaheiss@gmx.de>
2025-02-04 11:38:26 +0100

    fix(output.tf, networks.tf): Name Instanzen geändert, Security Group Name verändert

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,5 +1,5 @@
 # Ausgabe der öffentlichen IPs der Instanzen
 output "public_ips" {
-  value       = aws_instances.frontend-lt.*.public_ips
+  value       = aws_instances.nginx-asg-instance.*.public_ips
   description = "Öffentliche IP-Adressen der EC2-Instanzen im Frontend"
 }
