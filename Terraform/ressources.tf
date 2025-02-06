5b3df9a7cb58661a002f5578dbcd0f2239f809a7
Pia <piaheiss@gmx.de>
2025-02-06 09:30:22 +0100

    fix(Terraform/): depends_on hinzugefügt

--- Terraform/ressources.tf
+++ Terraform/ressources.tf
@@ -26,6 +26,7 @@ resource "aws_lb" "frontend_alb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
+  depends_on = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
 
   enable_deletion_protection = false
 }
@@ -36,6 +37,7 @@ resource "aws_lb_target_group" "frontend_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  depends_on = [ aws_vpc.main ]
 
   health_check {
     path                = "/"
@@ -52,6 +54,7 @@ resource "aws_lb_listener" "frontend_listener" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = 80
   protocol          = "HTTP"
+  depends_on = [ aws_lb_target_group.frontend_tg ]
 
   default_action {
     type             = "forward"
@@ -65,6 +68,7 @@ resource "aws_launch_template" "frontend_lt" {
   image_id      = "ami-07eef52105e8a2059" # Ubuntu User: ubuntu
   instance_type = "t2.micro"
   key_name      = "aws"
+  depends_on = [ aws_subnet.public_subnet_1 ]
 
    # User Data Script für Cloud-Init
    user_data = base64encode(file("${path.module}/userdata.tpl"))
@@ -87,6 +91,7 @@ resource "aws_autoscaling_group" "frontend_asg" {
   desired_capacity     = 2
   max_size             = 3
   min_size             = 1
+  depends_on = [ aws_launch_template.frontend_lt, aws_subnet.public_subnet_1, awaws_subnet.public_subnet_2, aws_subnet.public_subnet_3, aws_lb_target_group.frontend_tg ]
 
   launch_template {
     id      = aws_launch_template.frontend_lt.id
