00e41373f06588fad2af8f9afd4af28ae74cac3b
Pia <piaheiss@gmx.de>
2025-02-10 11:48:23 +0100

    fix(React-Router-DOM): React Router DOM implementiert

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,36 +7,57 @@ import Contact from './components/Contact/Contact';
 import Def from './components/Def/Def';
 import Map from './components/Map/Map';
 import Impressum from './components/Impressum/Impressum';
-import React from "react";
-import Sleepspots from './components/Sleepspots.jsx';
-import MapboxMap from './components/Map';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+// import React from "react";
+// import Sleepspots from './components/Sleepspots.jsx';
+// import MapboxMap from './components/Map';
 
 // Hier ist die Struktur der Website
 // Die verschiedenen Komponenten werden hier nacheinander aufgerufen
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.substring(1));
+      if (element) {
+        const yOffset = -150; // Offset von -100px
+        const yPosition =
+          element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+        window.scrollTo({ top: yPosition, behavior: 'smooth' });
+      }
+    }
+  }, [location]);
+
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <div className='container'>
-        <Title
-          subtitle='Suche deinen nächsten Stellplatz'
-          title='Stellplatz Sucher'
-        />
-        <Map />
-
+      <div id='home'>
+        <Navbar />
+        <Hero />
+      </div>
+      <div id='map'>
+        <div className='container'>
+          <Title
+            subtitle='Suche deinen nächsten Stellplatz'
+            title='Stellplatz Sucher'
+          />
+          <Map />
+        </div>
         <Contact />
-        <Title
-          subtitle='Du willst wissen wo es erlaubt ist zu stehen?'
-          title='Länder und ihre Regeln'
-        />
-        <Def />
+        <div id='definitionen'>
+          <Title
+            subtitle='Du willst wissen wo es erlaubt ist zu stehen?'
+            title='Länder und ihre Regeln'
+          />
+          <Def />
+        </div>
         <Laenderregeln />
 
-       {/* <Sleepspots/> */}
+        {/* <Sleepspots/> */}
       </div>
       <Fussleiste />
-      <Impressum />
     </div>
   );
 };
