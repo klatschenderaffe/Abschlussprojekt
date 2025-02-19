41be37c764b8abb9f51492a20da446628cc850e0
Pia <piaheiss@gmx.de>
2025-02-19 14:38:13 +0100

    Private Routen wieder auskommentiert

--- Frontend/src/ProtectedRoute.jsx
+++ Frontend/src/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from 'react-oidc-context';
+// import { Navigate, Outlet } from 'react-router-dom';
+// import { useAuth } from 'react-oidc-context';
 
-const ProtectedRoute = () => {
-  const auth = useAuth();
+// const ProtectedRoute = () => {
+//   const auth = useAuth();
 
-  const isLoggedIn = auth.isAuthenticated;
+//   const isLoggedIn = auth.isAuthenticated;
 
-  return isLoggedIn === 'true' ? <Outlet /> : <Navigate to='/' />;
-};
+//   return isLoggedIn === 'true' ? <Outlet /> : <Navigate to='/' />;
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
