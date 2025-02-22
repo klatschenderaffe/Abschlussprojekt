f42176f97eac4ab979d117a93210f7e37fb02b6f
Pia <piaheiss@gmx.de>
2025-02-22 14:59:03 +0100

    (Doku Pia): Tasks hinzugefügt

--- Frontend/src/components/Categorys/Categorys.jsx
+++ Frontend/src/components/Categorys/Categorys.jsx
@@ -1,5 +1,61 @@
+// import './Categorys.css';
+// import { Link } from 'react-router';
+// import Rostversiegelung from '../../assets/Rostversiegelung.png';
+// import Dämmung from '../../assets/Dämmung.png';
+// import MöbelBauen from '../../assets/MöbelBauen.png';
+// import FahrzeugAuswahl from '../../assets/FahrzeugAuswahl.png';
+// import BodenVerlegen from '../../assets/BodenVerlegen.png';
+// import WaendeVerkleiden from '../../assets/WaendeVerkleiden.png';
+
+// const Categorys = () => {
+
+//   return (
+//     <div className='category-container'>
+//       <div className='category-item'>
+//         <Link className='link' to='/blogpage'>
+//           <img src={FahrzeugAuswahl} alt='' />
+//           <h3 id='title'>Fahrzeug Auswahl</h3>
+//         </Link>
+//       </div>
+//       <div className='category-item'>
+//         <Link className='link' to='/'>
+//           <img src={Rostversiegelung} alt='' />
+//           <h3 id='title'>Rostversiegelung</h3>
+//         </Link>
+//       </div>
+//       <div className='category-item'>
+//         <Link className='link' to='/'>
+//           <img src={Dämmung} alt='' />
+//           <h3 id='title'>Dämmung</h3>
+//         </Link>
+//       </div>
+//       <div className='category-item'>
+//         <Link className='link' to='/'>
+//           <img src={BodenVerlegen} alt='' />
+//           <h3 id='title'>Boden verlegen</h3>
+//         </Link>
+//       </div>
+//       <div className='category-item'>
+//         <Link className='link' to='/'>
+//           <img src={WaendeVerkleiden} alt='' />
+//           <h3 id='title'>
+//             Wände und Decken <br /> verkleiden
+//           </h3>
+//         </Link>
+//       </div>
+//       <div className='category-item'>
+//         <Link className='link' to='/'>
+//           <img src={MöbelBauen} alt='' />
+//           <h3 id='title'>Möbel bauen</h3>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Categorys;
 import './Categorys.css';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom'; // Korrigiert: react-router -> react-router-dom
 import Rostversiegelung from '../../assets/Rostversiegelung.png';
 import Dämmung from '../../assets/Dämmung.png';
 import MöbelBauen from '../../assets/MöbelBauen.png';
@@ -11,41 +67,65 @@ const Categorys = () => {
   return (
     <div className='category-container'>
       <div className='category-item'>
-        <Link className='link' to='/blogpage'>
+        <Link
+          className='link'
+          to='/blogpage'
+          state={{ title: 'Fahrzeug Auswahl' }} // Titel übergeben
+        >
           <img src={FahrzeugAuswahl} alt='' />
-          <h3 id='title'>Fahrzeug Auswahl</h3>
+          <h3>Fahrzeug Auswahl</h3>
         </Link>
       </div>
       <div className='category-item'>
-        <Link className='link' to='/'>
+        <Link
+          className='link'
+          to='/blogpage'
+          state={{ title: 'Rostversiegelung' }} // Titel übergeben
+        >
           <img src={Rostversiegelung} alt='' />
-          <h3 id='title'>Rostversiegelung</h3>
+          <h3>Rostversiegelung</h3>
         </Link>
       </div>
       <div className='category-item'>
-        <Link className='link' to='/'>
+        <Link
+          className='link'
+          to='/blogpage'
+          state={{ title: 'Dämmung' }} // Titel übergeben
+        >
           <img src={Dämmung} alt='' />
-          <h3 id='title'>Dämmung</h3>
+          <h3>Dämmung</h3>
         </Link>
       </div>
       <div className='category-item'>
-        <Link className='link' to='/'>
+        <Link
+          className='link'
+          to='/blogpage'
+          state={{ title: 'Boden verlegen' }} // Titel übergeben
+        >
           <img src={BodenVerlegen} alt='' />
-          <h3 id='title'>Boden verlegen</h3>
+          <h3>Boden verlegen</h3>
         </Link>
       </div>
       <div className='category-item'>
-        <Link className='link' to='/'>
+        <Link
+          className='link'
+          to='/blogpage'
+          state={{ title: 'Wände und Decken verkleiden' }} // Titel übergeben
+        >
           <img src={WaendeVerkleiden} alt='' />
-          <h3 id='title'>
+          <h3>
             Wände und Decken <br /> verkleiden
           </h3>
         </Link>
       </div>
       <div className='category-item'>
-        <Link className='link' to='/'>
+        <Link
+          className='link'
+          to='/blogpage'
+          state={{ title: 'Möbel bauen' }} // Titel übergeben
+        >
           <img src={MöbelBauen} alt='' />
-          <h3 id='title'>Möbel bauen</h3>
+          <h3>Möbel bauen</h3>
         </Link>
       </div>
     </div>
